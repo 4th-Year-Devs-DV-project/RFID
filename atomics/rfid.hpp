4ad8021754fc3700960585ea878c7e51d8318f23
aryan <Aryan.r.t@hotmail.com>
2020-01-06 14:52:18 -0500

    Add files via upload

--- atomics/rfid.hpp
+++ atomics/rfid.hpp
@@ -9,6 +9,7 @@
 #ifndef BOOST_SIMULATION_PDEVS_RFID_HPP
 #define BOOST_SIMULATION_PDEVS_RFID_HPP
 
+#include <stdio.h>
 #include <cadmium/modeling/ports.hpp>
 #include <cadmium/modeling/message_bag.hpp>
 #include <limits>
@@ -24,7 +25,7 @@
 #include <limits>
 #include <random>
 #include "../drivers/MFRC522.cpp"
-#include <cadmium/real_time/arm_mbed/embedded_error.hpp>
+//#include <cadmium/real_time/arm_mbed/embedded_error.hpp>
 
 using namespace cadmium;
 using namespace std;
@@ -49,8 +50,9 @@ class Rfid
 
   Rfid(PinName sda, PinName scl, PinName scl2, PinName scl3, PinName scl4) 
   {
-    slowToggleTime  = TIME("00:00:05:00");
+    slowToggleTime    = TIME("00:00:20:00");
     state.temp_humid_sensor = new drivers::MFRC522(sda,scl,scl2,scl3,scl4);
+    state.newTag = false;
   }
     
   // state definition
@@ -67,30 +69,38 @@ class Rfid
 
   // internal transition
   void internal_transition() {
-  	printf("internal of RFID");
+  	//printf("newtag is %f", state.newTag);
   	state.newTag = false;
+    //sleep(50000);
+    
 
     for(int i =0; i <5; i++)
     {
-      if (state.temp_humid_sensor->PICC_IsNewCardPresent() && state.temp_humid_sensor->PICC_ReadCardSerial() ) 
+      if (state.temp_humid_sensor->PICC_IsNewCardPresent() ) 
       {
+        state.temp_humid_sensor->PICC_ReadCardSerial();
     
         state.newTag = true;
         state.id[0] = state.temp_humid_sensor->uid.uidByte[0];
         state.id[1] = state.temp_humid_sensor->uid.uidByte[1];
         state.id[2] = state.temp_humid_sensor->uid.uidByte[2];
         state.id[3] = state.temp_humid_sensor->uid.uidByte[3];
-        }
-        if( state.newTag == true)
-        {
+        //}
+      }
+        
+      if( state.newTag == true)
+      {
+          printf("-----------------------------------------------------------");
          	for (uint8_t i = 0; i < 4; i++)
           {
           	printf(" %X02", state.id[i]);
           }
           printf("\n\r");
-          break;
+          //break;
       }
-    }	    
+    }  
+    
+    
   }
 
 
@@ -101,16 +111,17 @@ class Rfid
   // confluence transition
   void confluence_transition(TIME e, typename make_message_bags<input_ports>::type mbs) {
     internal_transition();
-    external_transition(TIME(), std::move(mbs));
+    //external_transition(TIME(), std::move(mbs));
+    printf("here");
   }
 
   // output function
   typename make_message_bags<output_ports>::type output() const {
     typename make_message_bags<output_ports>::type bags;
     bool out;              
-    out = (state.newTag ? 1 : 0);
-    get_messages<typename defs::dataOut>(bags).push_back(out);
-                    
+    out = (state.newTag ? 1: 0);
+    //out = false;
+    get_messages<typename defs::dataOut>(bags).push_back(out);                    
     return bags;
   }
 
@@ -121,8 +132,8 @@ class Rfid
 
   friend std::ostringstream& operator<<(std::ostringstream& os, const typename Rfid<TIME>::state_type& i) {
     os << "Output: " << (i.newTag ? 1 : 0); 
+     //os << "Output: " << false; 
     return os;
   }
 };     
 #endif // BLINKY_HPP
-
